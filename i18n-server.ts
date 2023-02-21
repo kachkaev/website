f1d53cce958b875ffa31a19699fd9165a6fac5c0
Alexander Kachkaev <alexander@kachkaev.ru>
2023-02-21 09:15:28 +0000

    Change order of locales

--- i18n-server.ts
+++ i18n-server.ts
@@ -12,6 +12,6 @@ export const getDictionary = async (locale: Locale) =>
   dictionaryLookup[locale]();
 
 export const baseUrlByLocale: Record<Locale, string> = {
-  ru: process.env["BASE_URL_RU"] || "http://ru.localhost:3000",
   en: process.env["BASE_URL_EN"] || "http://localhost:3000",
+  ru: process.env["BASE_URL_RU"] || "http://ru.localhost:3000",
 };
