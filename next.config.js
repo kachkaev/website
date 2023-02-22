20020913fafae7093b69633e1098af0962a0ef97
Alexander Kachkaev <alexander@kachkaev.ru>
2023-02-22 19:14:53 +0000

    Skip linting during next build

--- next.config.js
+++ next.config.js
@@ -4,10 +4,18 @@
  * @type {import("next").NextConfig}
  */
 const nextConfig = {
+  output: "standalone",
   reactStrictMode: true,
+
   experimental: {
     appDir: true,
   },
+
+  // We call linters in GitHub Actions for all pull requests. By not linting
+  // again during `next build`, we save CI minutes and unlock more feedback.
+  // For local checks, run `pnpm run lint`.
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
 };
 
 export default nextConfig;
