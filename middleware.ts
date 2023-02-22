39113585f25092718e154b08b8abb3c22b847004
Alexander Kachkaev <alexander@kachkaev.ru>
2023-02-22 19:14:41 +0000

    Update matcher in middleware

--- middleware.ts
+++ middleware.ts
@@ -43,5 +43,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!_next/static|_next/image|favicon.ico|favicon/).*)"],
+  matcher: ["/((?!_next/static|_next/image|favicon.ico|favicon/|images/).*)"],
 };
