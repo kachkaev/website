705e7f0b32e5e0e16be530d0c104ac8dbbd16bc1
Alexander Kachkaev <alexander@kachkaev.ru>
2023-04-22 05:19:31 +0100

    Update tsconfig deps (compose them) (#1)

--- app/[locale]/layout/locale-switcher-inner.tsx
+++ app/[locale]/layout/locale-switcher-inner.tsx
@@ -4,7 +4,8 @@ import Link from "next/link";
 import { usePathname, useSearchParams } from "next/navigation";
 import * as React from "react";
 
-import { i18n, Locale } from "../../../i18n-config";
+import { type Locale } from "../../../i18n-config";
+import { i18n } from "../../../i18n-config";
 
 const localeHighlightLocalStorageKey = "hideLocaleHighlightUntil";
 
